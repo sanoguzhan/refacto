a18cd2f92a78da7a0225cbf5ef662018f2668a85
Oguzhan San <sanoguzhan@hotmail.com>
2021-01-18 18:22:25 +0100

    [wip] cmake for tests

---
+++ tests/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+// using namespace std;
+int main(void){
+
+    std::cout << "test" <<std::endl;
+    return 0;
+}
--- tests/test_dtables.cpp
+++ tests/test_dtables.cpp
@@ -1,8 +1,15 @@
-#include<iostream>
-#include ""
+#include <iostream>
+#include <yaml-cpp/yaml.h>
 
-int main(void){
+using namespace std;
 
+int main() {
+    YAML::Node test = YAML::LoadFile("test_data/test.yaml");
+    if (test["key_01"]) {
+      cout << "HELLO";
+    }else{
+      cout<< "test";
+    }
 
     return 0;
 }
