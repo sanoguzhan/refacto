06bbdee533014cebe0293bff79800ef80f1c4f25
Oguzhan San <sanoguzhan@hotmail.com>
2021-04-06 19:06:41 +0200

    [doc] readme

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 from parsing.engine import CSVParser
 from parsing.engine import XMLParser
 
-
+# CSVParser
+# Constructor input should be all string
 p = CSVParser({"csvparser" :{"path":"src/core/tests/test_data/csv/input/2110130777_treated_data.csv",
         "delim":";","skip":"0"}})
 
@@ -27,6 +28,9 @@ data = {
 
 p(data)
 
+#XMLParser
+# Expect number of dictionary as list
+# Name in the dictionary will be used to catogorized to file
 kwargs = [{"name": "inverter",
             "node":"Key",
             "key":"Pac",
@@ -55,6 +59,8 @@ kwargs = [{"name": "inverter",
 
 xml_parser = XMLParser(kwargs)
 
+# Here, pattern search for xml files are given, and the root name of each document
 xml_parser("src/core/tests/test_data/xml/*.xml", "WebBox");
 
+# Export location, it will write multiple files if there are more than one unique name in the kwargs dict
 xml_parser.to_csv(".");
