aeb11c8f5afae4656efa86190a329a61e8a189ca
michel srouji <msrouji@quantom.solar>
2022-01-26 13:56:04 +0100

    feat: add requirements for ci in Dockerfile

---
+++ docker/install_for_actions.sh
@@ -0,0 +1,7 @@
+# Install AWS cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+# Install Twine to upload package
+pip3 install twine
